1441a330f8cb64565c38cb0c1124025690b47c72
Silver <testingbugsrus@gmail.com>
2023-07-12 21:34:31 +0300

    Fix fixture load_env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
+from dotenv import load_dotenv
 from selene import browser
 
 
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
-    load_env()
+    load_dotenv()
+
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
