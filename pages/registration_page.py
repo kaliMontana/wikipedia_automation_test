adb73afc56e3fe9938c80e490fc67e3559d6f5a0
Silver <testingbugsrus@gmail.com>
2023-07-20 12:26:22 +0300

    - Refactoring user_main_page

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -3,6 +3,8 @@ import time
 
 from selene.support.shared import browser
 
+from utils.set_cookies import set_response_cookies_to_browser
+
 
 class RegistrationPage:
     def __init__(self, browser):
@@ -18,20 +20,8 @@ class RegistrationPage:
 
     def user_main_page(self, cookies):
         browser.open(self.user_main_page_path)
-        time.sleep(2)
+        time.sleep(1)
 
-        # logging.info(cookies.get('enwikiSession'))
-        # logging.info(cookies.get('NetworkProbeLimit'))
-        # logging.info(cookies.get('ss0-enwikiSession'))
-
-        browser.driver.add_cookie({'name': 'enwikiSession', 'value': cookies.get('enwikiSession')})
-        browser.driver.add_cookie({'name': 'enwikiUserID', 'value': cookies.get('enwikiUserID')})
-        browser.driver.add_cookie({'name': 'enwikiUserName', 'value': cookies.get('enwikiUserName')})
-        browser.driver.add_cookie({'name': 'loginnotify_prevlogins', 'value': cookies.get('loginnotify_prevlogins')})
-        browser.driver.add_cookie({'name': 'cpPosIndex', 'value': cookies.get('cpPosIndex')})
-        browser.driver.add_cookie({'name': 'UseDC', 'value': cookies.get('UseDC')})
-        browser.driver.add_cookie({'name': 'NetworkProbeLimit', 'value': cookies.get('NetworkProbeLimit')})
-        browser.driver.add_cookie({'name': 'centralauth_User', 'value': cookies.get('centralauth_User')})
-        browser.driver.add_cookie({'name': 'centralauth_Session', 'value': cookies.get('centralauth_Session')})
+        set_response_cookies_to_browser(cookies)
 
         browser.open(self.user_main_page_path)
