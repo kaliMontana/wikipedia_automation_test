3af90c903a0c8bad16e26db4265ecb1417705f17
Silver <testingbugsrus@gmail.com>
2023-07-21 15:12:10 +0300

    - Add allure tags to tests

--- tests/test_functionalities.py
+++ tests/test_functionalities.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.api_pages import RegistarionThroughtApi
 from pages.registration_page import RegistrationPage
 from pages.user_main_page import UserMainPage
@@ -7,6 +9,8 @@ user_main_page = UserMainPage()
 registarionThroughtApi = RegistarionThroughtApi()
 
 
+@allure.feature('User interface authorization')
+@allure.title('Authorization')
 def test_authorization_ui():
     registration_page.open_login_page()
     registration_page.set_login()
@@ -15,6 +19,8 @@ def test_authorization_ui():
     user_main_page.should_have_welcome_and_name()
 
 
+@allure.feature('Api authorization')
+@allure.title('Authorization')
 def test_authorization_through_api():
     login_page_response = registarionThroughtApi.request_login_page()
     reponse_wpLoginToken = registarionThroughtApi.get_loginToken_from_response(login_page_response)
