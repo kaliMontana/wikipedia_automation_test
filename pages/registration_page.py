ac2342a0060539c50cf19eb1859a1f934057407d
Silver <testingbugsrus@gmail.com>
2023-07-21 09:54:59 +0300

    - Add steps to registration through ui

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,24 +1,38 @@
-import logging
+import os
 import time
 
+from dotenv import load_dotenv
 from selene.support.shared import browser
 
 from utils.set_cookies import set_response_cookies_to_browser
 
 
 class RegistrationPage:
-    def __init__(self, browser):
+    load_dotenv()
+
+    def __init__(self):
         self.log_in_page_path = '/w/index.php?title=Special:UserLogin&returnto=Main+Page'
         self.user_main_page_path = '/wiki/Main_Page'
+        self.login = browser.element('input[id=wpName1]')
+        self.password = browser.element('input[id=wpPassword1]')
+        self.login_attempt_button = browser.element('button[id=wpLoginAttempt]')
 
-    def log_in(self, browser):
+    def log_in(self):
         browser.open(self.log_in_page_path)
 
-    def login_ui(self, browser):
+    def open_login_page(self):
         browser.open(self.log_in_page_path)
-        browser
 
-    def user_main_page(self, cookies):
+    def set_login(self):
+        self.login.send_keys(os.getenv('wplogin'))
+
+    def set_password(self):
+        self.password.send_keys(os.getenv('wpPassword'))
+
+    def login_attempt(self):
+        self.login_attempt_button.click()
+
+    def set_cookies_in_user_main_page(self, cookies):
         browser.open(self.user_main_page_path)
         time.sleep(1)
 
