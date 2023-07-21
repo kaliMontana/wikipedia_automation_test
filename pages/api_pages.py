23d4ac330e64147f7ef8aa27b08a5e55ff548389
Silver <testingbugsrus@gmail.com>
2023-07-21 15:32:36 +0300

    - Refactor names in RegistrationThroughApi

--- pages/api_pages.py
+++ pages/api_pages.py
@@ -1,13 +1,13 @@
-from utils.api_utils import request_to_login_page, get_loginToken_from_response, request_for_authorization
+from utils.api_utils import request_to_login_page, request_for_authorization, get_loginToken_from_response
 
 
-class RegistarionThroughtApi():
+class RegistrationThroughApi():
 
     def request_login_page(self):
         login_page_response = request_to_login_page()
         return login_page_response
 
-    def get_loginToken_from_response(self, login_page_response):
+    def get_loginToken(self, login_page_response):
         reponse_wpLoginToken = get_loginToken_from_response(login_page_response)
         return reponse_wpLoginToken
 
