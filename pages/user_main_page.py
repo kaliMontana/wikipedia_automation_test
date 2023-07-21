24584e9d0b5c8395b3f7186b9df65817b49e0601
Silver <testingbugsrus@gmail.com>
2023-07-21 15:52:58 +0300

    - Add tag @allure.step to steps

--- pages/user_main_page.py
+++ pages/user_main_page.py
@@ -1,5 +1,6 @@
 import os
 
+import allure
 from dotenv import load_dotenv
 from selene import have
 from selene.support.shared.jquery_style import s
@@ -12,6 +13,7 @@ class UserMainPage:
         self.welcome_wp = s('span[id=Welcome_to_Wikipedia]')
         self.your_home_page = s("a[title='Your homepage (page does not exist) [alt-shift-.]'] span")
 
+    @allure.step('Check welcome title and user name in user main page')
     def should_have_welcome_and_name(self):
         assert self.welcome_wp.should(have.text('Welcome to'))
         assert self.your_home_page.should(have.exact_text(os.getenv('wplogin')))
