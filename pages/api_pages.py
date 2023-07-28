16d714cc585dd3abc8a494cf7882dd918ca5e41e
Silver <testingbugsrus@gmail.com>
2023-07-28 11:26:26 +0300

    - CAdd steps requests_parameters, check_page_title_and_url

--- pages/api_pages.py
+++ pages/api_pages.py
@@ -1,9 +1,18 @@
-import allure
+import html
+import logging
 
-from utils.api_utils import request_to_login_page, request_for_authorization, get_loginToken_from_response
+import allure
+from bs4 import BeautifulSoup
+
+from tests.conftest import wiki_base_url
+from utils.api_request_utils import request_to_login_page, request_for_authorization, get_loginToken_from_response, \
+    request_for_article_search
 
 
 class RegistrationThroughApi():
+    def __init__(self):
+        self.wiki_path = '/wiki/'
+        self.page_article_title_selector = 'h1[id=firstHeading] span[class*=page-title-main]'
 
     @allure.step('Send request to lon in page')
     def request_login_page(self):
@@ -19,3 +28,18 @@ class RegistrationThroughApi():
     def request_authorization(self, reponse_wpLoginToken, cookies):
         authorization_response = request_for_authorization(reponse_wpLoginToken, cookies)
         return authorization_response
+
+    @allure.step('Send request to search an article')
+    def request_article_search(self, cookies):
+        article_search_response = request_for_article_search(cookies)
+        logging.info(article_search_response.url)
+        return article_search_response
+
+    @allure.step('Check url and tab, page titles')
+    def check_page_title_and_url(self, article_search_response, page_article_title, word_to_search):
+        response_parser = BeautifulSoup(html.unescape(article_search_response.text), 'html.parser')
+        title_for_url = word_to_search.replace(' ', '_')
+
+        assert article_search_response.url == f'{wiki_base_url}{self.wiki_path}{title_for_url}'
+        assert response_parser.select('title')[0].text == page_article_title
+        assert response_parser.select(self.page_article_title_selector)[0].text == word_to_search
