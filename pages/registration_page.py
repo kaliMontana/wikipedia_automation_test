d257e1bb9f081c3f58aff8d7d7030917965d2fe9
Silver <testingbugsrus@gmail.com>
2023-08-06 22:31:04 +0300

    - Refactoring RegistrationPage

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -4,9 +4,6 @@ import time
 import allure
 from dotenv import load_dotenv
 
-"""from tests.conftest import browser_setup
-from selene.support.shared import browser"""
-
 from utils.api_utils import set_response_cookies_to_browser
 
 
@@ -14,28 +11,29 @@ class RegistrationPage:
     load_dotenv()
 
     def __init__(self, browser):
+        self.browser = browser
         self.log_in_page_path = '/w/index.php'
         self.log_in_page_auth_query = '?title=Special:UserLogin&returnto=Main+Page'
         self.user_main_page_path = '/wiki/Main_Page'
-        self.login = browser.element('input[id=wpName1]')
-        self.password = browser.element('input[id=wpPassword1]')
-        self.login_attempt_button = browser.element('button[id=wpLoginAttempt]')
+        self.login = 'input[id=wpName1]'
+        self.password = 'input[id=wpPassword1]'
+        self.login_attempt_button = 'button[id=wpLoginAttempt]'
 
     @allure.step('Open "Log in" page')
-    def open_login_page(self, browser):
-        browser.open(f'{self.log_in_page_path}{self.log_in_page_auth_query}')
+    def open_login_page(self):
+        self.browser.open(f'{self.log_in_page_path}{self.log_in_page_auth_query}')
 
     @allure.step('Type login')
     def set_login(self):
-        self.login.send_keys(os.getenv('wplogin'))
+        self.browser.element(self.login).send_keys(os.getenv('wplogin'))
 
     @allure.step('Type password')
     def set_password(self):
-        self.password.send_keys(os.getenv('wpPassword'))
+        self.browser.element(self.password).send_keys(os.getenv('wpPassword'))
 
     @allure.step('Click on button Log in')
     def login_attempt(self):
-        self.login_attempt_button.click()
+        self.browser.element(self.login_attempt_button).click()
 
     @allure.step('Open user main page')
     def open_user_main_page_and_set_cookies(self, browser, cookies):
