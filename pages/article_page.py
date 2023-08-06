49b696cbbf31183f17eeeb517e843d83802781de
Silver <testingbugsrus@gmail.com>
2023-08-06 22:52:58 +0300

    - Refactoring UserMainPage, ArticlePage

--- pages/article_page.py
+++ pages/article_page.py
@@ -1,37 +1,37 @@
 import allure
 from selene import have, be
 from selene.core.exceptions import TimeoutException
-from selene.support.shared.jquery_style import s
 
 
 class ArticlePage:
 
-    def __init__(self):
-        self.page_title_main = s('h1[id=firstHeading] span[class*=page-title-main]')
-        self.add_article_to_watchlist = s('#ca-watch a[title*="Add this page"]')
-        self.delete_article_to_watchlist_unstart = s('#ca-unwatch a[title*="Remove this page"]')
-        self.go_to_watch_list_page = s("#pt-watchlist-2 a[title*='The list of pages']")
+    def __init__(self, browser):
+        self.browser = browser
+        self.page_title_main = 'h1[id=firstHeading] span[class*=page-title-main]'
+        self.add_article_to_watchlist = '#ca-watch a[title*="Add this page"]'
+        self.delete_article_to_watchlist_unstart = '#ca-unwatch a[title*="Remove this page"]'
+        self.go_to_watch_list_page = "#pt-watchlist-2 a[title*='The list of pages']"
         self.add_star_icon_selector = '#ca-watch a[title*="Add this page"]'
 
     @allure.step('Check that the article page have title {title}')
     def check_page_title(self, title):
-        self.page_title_main.should(have.exact_text(title))
+        self.browser.element(self.page_title_main).should(have.exact_text(title))
 
     @allure.step('Add article to watch list')
     def add_article(self):
         if self.is_element_present(self.add_star_icon_selector):
-            self.add_article_to_watchlist.click()
+            self.browser.element(self.add_article_to_watchlist).click()
         else:
-            self.delete_article_to_watchlist_unstart.click()
-            self.add_article_to_watchlist.click()
+            self.browser.element(self.delete_article_to_watchlist_unstart).click()
+            self.browser.element(self.add_article_to_watchlist).click()
 
     @allure.step('Go to watch list')
     def go_to_watch_list(self):
-        self.go_to_watch_list_page.click()
+        self.browser.element(self.go_to_watch_list_page).click()
 
     def is_element_present(self, selector):
         try:
-            s(selector).should(be.present)
+            self.browser.element(selector).should(be.present)
             return True
         except TimeoutException:
             return False
--- pages/user_main_page.py
+++ pages/user_main_page.py
@@ -3,24 +3,26 @@ import os
 import allure
 from dotenv import load_dotenv
 from selene import have
-from selene.support.shared.jquery_style import s
 
 
 class UserMainPage:
     load_dotenv()
 
-    def __init__(self):
-        self.welcome_wp = s('span[id=Welcome_to_Wikipedia]')
-        self.your_home_page = s("a[title='Your homepage (page does not exist) [alt-shift-.]'] span")
-        self.search_input = s('form[id=searchform] input[class~=cdx-text-input__input]')
-        self.search_button = s('#searchform button[class~=cdx-button]')
+    def __init__(self, browser):
+        self.browser = browser
+        self.welcome_wp = 'span[id=Welcome_to_Wikipedia]'
+        self.your_home_page = "a[title='Your homepage (page does not exist) [alt-shift-.]'] span"
+        self.search_input = 'form[id=searchform] input[class~=cdx-text-input__input]'
+        self.search_button = '#searchform button[class~=cdx-button]'
+        self.welcome_to = 'Welcome to'
+        self.wplogin = 'wplogin'
 
     @allure.step('Check welcome title and user name in user main page')
     def should_have_welcome_and_user_name(self):
-        assert self.welcome_wp.should(have.text('Welcome to'))
-        assert self.your_home_page.should(have.exact_text(os.getenv('wplogin')))
+        assert self.browser.element(self.welcome_wp).should(have.text(self.welcome_to))
+        assert self.browser.element(self.your_home_page).should(have.exact_text(os.getenv(self.wplogin)))
 
     @allure.step("Search '{search}'")
     def search(self, search):
-        self.search_input.type(search)
-        self.search_button.click()
+        self.browser.element(self.search_input).type(search)
+        self.browser.element(self.search_button).click()
