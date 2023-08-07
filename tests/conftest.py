5890d12ccd60d538e6649684dcdd26a3e4e6ef8d
Silver <testingbugsrus@gmail.com>
2023-08-07 12:46:50 +0300

    - Refactor pytest_addoption and browser_setup to local and remote starts

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,9 @@ DEFAULT_BROWSER_VERSION = "100.0"
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--browser_version',
-        default='100.0'
+        '--browser_version', default='100.0')
+    parser.addoption(
+        '--environment', default='remote'
     )
 
 
@@ -26,37 +27,46 @@ def load_env():
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup(request):
-    # browser.config.window_width = 1920
-    # browser.config.window_height1080 = 1080
-    # browser.config.base_url = wiki_base_url
-    # yield
-    # browser.quit()
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+    if request.config.getoption('--environment') == 'local':
+        from selene import browser
+
+        browser.config.window_width = 1920
+        browser.config.window_height1080 = 1080
+        browser.config.base_url = wiki_base_url
+        yield browser
+
+        attach.add_html(browser)
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_video(browser)
+        browser.quit()
+    else:
+        browser_version = request.config.getoption('--browser_version')
+        browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
         }
-    }
-    options.capabilities.update(selenoid_capabilities)
+        options.capabilities.update(selenoid_capabilities)
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
 
-    driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub/",
-        options=options
-    )
-    browser = Browser(Config(driver=driver, base_url=wiki_base_url, window_width=1920, window_height=1080))
+        driver = webdriver.Remote(
+            command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub/",
+            options=options
+        )
+        browser = Browser(Config(driver=driver, base_url=wiki_base_url, window_width=1920, window_height=1080))
 
-    yield browser
+        yield browser
 
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-    browser.quit()
+        attach.add_html(browser)
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_video(browser)
+        browser.quit()
